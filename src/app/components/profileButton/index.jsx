26b4d623b911eae6db9a301c9057ee3ea426290a
Wendell47 <ps4.wendell@gmail.com>
2023-05-31 17:56:32 -0400

    add icons animation

--- src/app/components/profileButton/index.jsx
+++ src/app/components/profileButton/index.jsx
@@ -2,7 +2,7 @@ import {HiChevronRight} from 'react-icons/hi'
 
 export default function ProfileButton({icon: Icon, title, ...rest}){
     return(
-        <a {...rest} className="text-gray-700 inline-flex justify-between items-center rounded-lg p-2 transition hover:bg-slate-200 hover:text-gray-700/90" href="#">
+        <a {...rest} className="btn text-gray-700 inline-flex justify-between items-center rounded-lg p-2 transition hover:bg-slate-200 hover:text-gray-700/90" href="#">
             <span className='inline-flex items-center gap-2'> {Icon && <Icon size={20}/>} {title}</span> <HiChevronRight className='fill-gray-400' /> </a>
     )
 }
